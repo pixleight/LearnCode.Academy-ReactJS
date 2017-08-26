0a1025ee19438bc5911fb0d7007d2090e0ca2201
Chris Violette <cvioletterun@gmail.com>
2017-08-26 11:35:21 -0400

    REACT JS TUTORIAL #4 - State vs Props & Application Data

--- 1-basic-react/src/js/components/Header.js
+++ 1-basic-react/src/js/components/Header.js
@@ -6,7 +6,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <header>
-        <Title/>
+        <Title title={this.props.title} />
       </header>
     );
   }
--- 1-basic-react/src/js/components/Layout.js
+++ 1-basic-react/src/js/components/Layout.js
@@ -4,10 +4,21 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 export default class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "Welcome",
+    };
+  }
   render() {
+    setTimeout(() => {
+      this.setState({title: "Welcome Chris!"})
+    }, 2000);
+    
     return (
       <div>
-        <Header/>
+        <Header title={this.state.title} />
+        <Header title="Other Title" />
         <Footer/>
       </div>
     );
