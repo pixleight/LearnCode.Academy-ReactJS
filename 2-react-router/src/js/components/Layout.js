ee86ae94992f5441f5116b52fe20d0b5a0324022
Chris Violette <cvioletterun@gmail.com>
2017-08-26 15:57:47 -0400

    react-router basic setup

--- 2-react-router/src/js/components/Layout.js
+++ 2-react-router/src/js/components/Layout.js
@@ -1,29 +1,9 @@
 import React from "react";
 
-import Footer from "./Footer";
-import Header from "./Header";
-
 export default class Layout extends React.Component {
-  constructor() {
-    super();
-    // Create state & set default title. Accessed via this.state.title
-    this.state = {
-      title: "Welcome",
-    };
-  }
-
-  // Function to change this.state.title
-  changeTitle(title) {
-    this.setState({title});
-  }
-
   render() {
-    // Add changeTitle to Header element. Make sure to .bind(this) to use function in this component
     return (
-      <div>
-        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
-        <Footer/>
-      </div>
+      <h1>KillerNews.net</h1>
     );
   }
 }
