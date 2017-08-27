dbd77ff854fb003c3fe426f4fc250eee6fa1d019
Chris Violette <cvioletterun@gmail.com>
2017-08-27 18:52:37 -0400

    Setting up 3-flux

---
+++ 3-flux/src/js/pages/Layout.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router";
+
+import Footer from "../components/layout/Footer";
+import Nav from "../components/layout/Nav";
+
+export default class Layout extends React.Component {
+  render() {
+    const { history } = this.props;
+    const containerStyle = {
+      marginTop: "60px"
+    };
+
+    return (
+      <div>
+        <Nav/>
+        <div class="container" style={containerStyle}>
+          <div class="row">
+            <div class="col-lg-12">
+              <h1>Todo App</h1>
+              {this.props.children}
+            </div>
+          </div>
+          <Footer/>
+        </div>
+        {/* /.container */}
+      </div>
+    );
+  }
+}
