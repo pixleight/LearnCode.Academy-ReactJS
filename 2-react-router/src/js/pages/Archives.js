0dd4b23cc0f5742a975a06b148aef44432158e7f
Chris Violette <cvioletterun@gmail.com>
2017-08-27 11:12:31 -0400

    React JS Tutorial #7 - React Router Params & Queries

--- 2-react-router/src/js/pages/Archives.js
+++ 2-react-router/src/js/pages/Archives.js
@@ -2,8 +2,17 @@ import React from "react";
 
 export default class Archives extends React.Component {
   render() {
+    // Getting url params, eg /archives/some-article
+    const { params } = this.props;
+    const { article } = params;
+    // Getting url queries, eg ?date=today&filter=none
+    const { query } = this.props.location;
+    const { date, filter } = query;
     return (
-      <h1>Archives</h1>
+      <div>
+        <h1>Archives ({article})</h1>
+        <h4>date: {date}, filter: {filter}</h4>
+      </div>
     );
   }
 }
--- 2-react-router/src/js/pages/Layout.js
+++ 2-react-router/src/js/pages/Layout.js
@@ -5,12 +5,14 @@ export default class Layout extends React.Component {
   navigate() {
     this.props.history.pushState(null, "/");
   }
-  
+
   render() {
+    const { history } = this.props;
+    console.log(history.isActive("archives"));
     return (
       <div>
         <h1>KillerNews.net</h1>
-        <Link to="archives">archives</Link>
+        <Link to="archives" activeClassName="test">archives</Link>
         <Link to="settings">settings</Link>
         <button onClick={this.navigate.bind(this)}>featured</button>
         {this.props.children}
