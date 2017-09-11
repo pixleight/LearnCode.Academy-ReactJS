ab9e083f5173257dfc3bf0a113cd8ee1ca1c9784
Chris Violette <cvioletterun@gmail.com>
2017-09-10 21:56:29 -0400

    Redux introduction

--- 4-redux/src/js/client.js
+++ 4-redux/src/js/client.js
@@ -1,7 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createStore } from "redux";
 
-import Layout from "./components/Layout";
+// Create a reducer to handle various actions
+const reducer = function(state, action) {
+  if (action.type === "INC") {
+    return state+action.payload;
+  }
+  if (action.type === "DEC") {
+    return state-action.payload;
+  }
+  return state;
+}
 
-const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+// Create a store to handle states
+const store = createStore(reducer, 0);
+store.subscribe(() => {
+  console.log("store changed", store.getState());
+});
+
+// Dispatch actions and payloads to the reducer
+store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "INC", payload: 2});
+store.dispatch({type: "INC", payload: 22});
+store.dispatch({type: "DEC", payload: 1});
+store.dispatch({type: "DEC", payload: 13});
+store.dispatch({type: "DEC", payload: 3});
